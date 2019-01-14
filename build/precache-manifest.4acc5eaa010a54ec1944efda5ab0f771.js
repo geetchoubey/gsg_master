b82cc082d15d77c961bd2e18109a3d4ab9a2f476
Geet Choubey <geetchoubey@gmail.com>
2019-01-14 21:42:25 +0530

    Removed build folder from gitignore

---
+++ build/precache-manifest.4acc5eaa010a54ec1944efda5ab0f771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81a60ce652ac811763f8",
+    "url": "/static/js/main.81a60ce6.chunk.js"
+  },
+  {
+    "revision": "88b05f1f37f869a089d2",
+    "url": "/static/js/1.88b05f1f.chunk.js"
+  },
+  {
+    "revision": "81a60ce652ac811763f8",
+    "url": "/static/css/main.ae410148.chunk.css"
+  },
+  {
+    "revision": "3e34e1407897a78c657a3a5605ebac80",
+    "url": "/index.html"
+  }
+];
