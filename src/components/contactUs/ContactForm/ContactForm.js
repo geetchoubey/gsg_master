66399c5b121ef6dea5652be8a5198d14a43cec2f
azharuddin khan <azharuddinkhan8988@gmail.com>
2019-02-23 01:48:51 +0530

    added mobile hamberger menu and some changes

--- src/components/contactUs/ContactForm/ContactForm.js
+++ src/components/contactUs/ContactForm/ContactForm.js
@@ -3,6 +3,7 @@ import React from "react";
 const ContactForm = (props) => {
     return(
         <div className="form-wrapper">
+            <div className="form-container">
             <h1>CONTACT US</h1>
             <p className="center">We are always here to help out whatever way we can.</p>
             <div className="form">
@@ -12,6 +13,8 @@ const ContactForm = (props) => {
                 <textarea name="message" cols="30" rows="4" placeholder="Type your message"></textarea>
                 <button className="submit">Submit</button>
             </div>
+            </div>
+            
         </div>
     )
 }
--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ContactMap from "./ContactMap/ContactMap"
-import ContactForm from "./ContactForm/ContactForm"
+import ContactForm from "./ContactForm/ContactForm";
+import Helmet from "react-helmet"
 
 class ContactUs extends React.Component{
     
     render(){
         
         return(
+            <React.Fragment>
+            <Helmet>
+                <title>Getsetgig | Contact us</title>
+            </Helmet>
             <div className="contact-wrapper top-header-space">
                 <div className="row">
                     <div className="col-md-6">
@@ -17,6 +22,7 @@ class ContactUs extends React.Component{
                     </div>
                 </div>
             </div>
+            </React.Fragment>
         )
     }
 }
