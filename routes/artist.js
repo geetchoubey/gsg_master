515d79e4df84a9de2c8b206aa4f90c6785553f7d
Geet Choubey <geetchoubey@gmail.com>
2019-03-12 13:57:52 +0530

    Added S3 bucket integration to image upload for artist

--- routes/artist.js
+++ routes/artist.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var aws = require('aws-sdk');
+var multerS3 = require('multer-s3');
 
 const db = require('../AssocationModels');
 const AdminAuth = require('./auth/admin');
@@ -28,172 +30,158 @@ const multer = require('multer');
 const Helper = require('../utils/Helper');
 
 router.get('/', [AdminAuth], (req, res, next) => {
-  db.artist.findAll({
-    include: [{
-      model: Genres,
-      as: 'ArtistGenre',
-      through: {attributes: []},
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: Skills,
-      as: 'ArtistSkill',
-      through: {attributes: []},
-      attributes: ['id'],
-      required: false,
-    }, {
-      model: Logo,
-      as: 'logo',
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: ArtistType,
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: UserModel,
-      attributes: {
-        exclude: ['password', 'id', 'auth_token', 'remember_token', 'role_id']
-      }
-    }, {
-      model: Ratings,
-      as: 'ratings',
-      attributes: {
-        exclude: ['rating', 'created_at', 'updated_at', 'artist_id'],
-        include: [[sequelize.fn('AVG', sequelize.col('rating')), 'rating']]
-      }
-    }],
-    subQuery: false,
-    order: [
-      ['created_at', 'DESC']
-    ],
-    group: ['artist.id', 'ArtistGenre.id', 'ArtistSkill.id'],
-    attributes: {
-      exclude: ['user_id', 'logo_id', 'artist_type_id']
-    },
-    limit: req.query.limit,
-    offset: req.skip
-  }).then(artists => {
-    var results = {};
-    results.rows = artists;
-    db.artist.count().then(count => {
-      results.count = count;
-      res.send(db.paginate(req, results));
-    });
+    db.artist.findAll({
+        include: [{
+            model: Genres,
+            as: 'ArtistGenre',
+            through: {attributes: []},
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: Skills,
+            as: 'ArtistSkill',
+            through: {attributes: []},
+            attributes: ['id'],
+            required: false,
+        }, {
+            model: Logo,
+            as: 'logo',
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: ArtistType,
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: UserModel,
+            attributes: {
+                exclude: ['password', 'id', 'auth_token', 'remember_token', 'role_id']
+            }
+        }, {
+            model: Ratings,
+            as: 'ratings',
+            attributes: {
+                exclude: ['rating', 'created_at', 'updated_at', 'artist_id'],
+                include: [[sequelize.fn('AVG', sequelize.col('rating')), 'rating']]
+            }
+        }],
+        subQuery: false,
+        order: [
+            ['created_at', 'DESC']
+        ],
+        group: ['artist.id', 'ArtistGenre.id', 'ArtistSkill.id'],
+        attributes: {
+            exclude: ['user_id', 'logo_id', 'artist_type_id']
+        },
+        limit: req.query.limit,
+        offset: req.skip
+    }).then(artists => {
+        var results = {};
+        results.rows = artists;
+        db.artist.count().then(count => {
+            results.count = count;
+            res.send(db.paginate(req, results));
+        });
 
-  });
+    });
 });
 
 router.get('/:slug', (req, res, next) => {
-  db.artist.findAll({
-    include: [{
-      model: Genres,
-      as: 'ArtistGenre',
-      through: {attributes: []},
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: Skills,
-      as: 'ArtistSkill',
-      through: {attributes: []},
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: Logo,
-      as: 'logo',
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: ArtistType,
-      attributes: {
-        exclude: ['id']
-      }
-    }, {
-      model: UserModel,
-      attributes: {
-        exclude: ['password', 'id', 'auth_token', 'remember_token', 'role_id']
-      }
-    }, {
-      model: Ratings,
-      as: 'ratings',
-      attributes: {
-        exclude: ['rating', 'created_at', 'updated_at', 'artist_id'],
-        include: [[sequelize.fn('AVG', sequelize.col('rating')), 'rating']]
-      }
-    }],
-    // raw: true,
-    where: {
-      slug: req.params.slug
-    }, subQuery: false,
-    group: ['id', 'ArtistGenre.id', 'ArtistGenre->artist_genre.id', 'ArtistSkill.id', 'ArtistSkill->artist_skill.id'],
-    attributes: {
-      exclude: ['created_at', 'updated_at', 'user_id', 'logo_id', 'artist_type_id']
-    }
-  }).then(artist => {
-    if (!artist[0]) {
-      res.sendStatus(404);
-      return;
-    }
-    res.send(artist[0]);
+    db.artist.findAll({
+        include: [{
+            model: Genres,
+            as: 'ArtistGenre',
+            through: {attributes: []},
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: Skills,
+            as: 'ArtistSkill',
+            through: {attributes: []},
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: Logo,
+            as: 'logo',
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: ArtistType,
+            attributes: {
+                exclude: ['id']
+            }
+        }, {
+            model: UserModel,
+            attributes: {
+                exclude: ['password', 'id', 'auth_token', 'remember_token', 'role_id']
+            }
+        }, {
+            model: Ratings,
+            as: 'ratings',
+            attributes: {
+                exclude: ['rating', 'created_at', 'updated_at', 'artist_id'],
+                include: [[sequelize.fn('AVG', sequelize.col('rating')), 'rating']]
+            }
+        }],
+        // raw: true,
+        where: {
+            slug: req.params.slug
+        }, subQuery: false,
+        group: ['id', 'ArtistGenre.id', 'ArtistGenre->artist_genre.id', 'ArtistSkill.id', 'ArtistSkill->artist_skill.id'],
+        attributes: {
+            exclude: ['created_at', 'updated_at', 'user_id', 'logo_id', 'artist_type_id']
+        }
+    }).then(artist => {
+        if (!artist[0]) {
+            res.sendStatus(404);
+            return;
+        }
+        res.send(artist[0]);
 
-  }).catch(err => {
-    global.logger(err);
-    res.sendStatus(500);
-  })
-});
-
-router.get('/test', (req, res, next) => {
-  db.rating.findAll({
-    where: {
-      artist_id: 1
-    },
-    group: ['artist_id'],
-    attributes: ['artist_id', [sequelize.fn('AVG', sequelize.col('rating')), 'avgValue']]
-  }).then(rating => {
-    res.send(rating);
-  }).catch(err => {
-    console.log(err);
-  });
+    }).catch(err => {
+        global.logger(err);
+        res.sendStatus(500);
+    })
 });
 
 // Create New Artist
 router.post('/:userId', (req, res, next) => {
-  db.user.findOne({
-    where: {
-      id: Helper.decrypt(req.params.userId)
-    }
-  }).then(user => {
-    if (!user) {
-      res.sendStatus(404);
-      return;
-    }
-    db.artist.findOrCreate({
-      where: {
-        user_id: Helper.decrypt(req.params.userId)
-      }, defaults: {
-        // Set other body values
-        name: req.body.name,
-        slug: req.body.name,
-        location: req.body.location,
-        description: req.body.description,
-        gender: req.body.gender,
-        user_id: user.dataValues.id,
-        artist_type_id: req.body.artistTypeId
-      }
-    }).spread((artist, created) => {
-      if (!created) {
-        res.sendStatus(409);
-        return;
-      }
-      res.status(201).send(artist);
+    db.user.findOne({
+        where: {
+            id: Helper.decrypt(req.params.userId)
+        }
+    }).then(user => {
+        if (!user) {
+            res.sendStatus(404);
+            return;
+        }
+        db.artist.findOrCreate({
+            where: {
+                user_id: Helper.decrypt(req.params.userId)
+            }, defaults: {
+                // Set other body values
+                name: req.body.name,
+                slug: req.body.name,
+                location: req.body.location,
+                description: req.body.description,
+                gender: req.body.gender,
+                user_id: user.dataValues.id,
+                artist_type_id: req.body.artistTypeId
+            }
+        }).spread((artist, created) => {
+            if (!created) {
+                res.sendStatus(409);
+                return;
+            }
+            res.status(201).send(artist);
+        });
     });
-  });
 });
 
 router.use('/:slug/bankdetails', BankDetailsAPI);
@@ -203,44 +191,51 @@ router.use('/:slug/audios', AudioAPI);
 router.use('/:slug/videos', VideoAPI);
 router.use('/:slug/ratings', RatingAPI);
 
+var s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+});
+
 var upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, Helper.encrypt(Date.now()) + path.extname(file.originalname));
-    }
-  })
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME || '',
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, (Date.now().toString() + path.extname(file.originalname)))
+        }
+    })
 }).single('logo');
 
-router.post('/:slug/logo', [ArtistAuth], (req, res, next) => {
-  db.artist.findOne({
-    where: {
-      slug: req.params.slug
-    }
-  }).then(artist => {
+router.post('/:slug/logo', [ArtistAuth], async (req, res, next) => {
+    const artist = await db.artist.findOne({
+        where: {
+            slug: req.params.slug
+        }
+    });
     if (!artist) {
-      res.sendStatus(404);
-      return;
+        res.sendStatus(404);
+        return;
     }
     upload(req, res, err => {
 
-      if (err) {
-        res.sendStatus(400);
-        return;
-      }
-      db.image.create({
-        url: req.file.filename
-      }).then(image => {
-        artist.setLogo(image);
-        res.sendStatus(201);
-      }).catch(err => {
-        res.sendStatus(400);
-        global.logger(err);
-      });
+        if (err) {
+            res.sendStatus(400);
+            return;
+        }
+        db.image.create({
+            url: req.file.filename
+        }).then(image => {
+            artist.setLogo(image);
+            res.sendStatus(201);
+        }).catch(err => {
+            res.sendStatus(400);
+            global.logger(err);
+        });
     });
-  });
 });
 
 module.exports = router;
